9df4d4cf483cadc45f15b568eebdf22d037ac6e4
Tony Stone <hatter24@gmail.com>
2016-11-22 17:06:12 -0800

    Adding Segment.swift to define basic Segment class for geometric calculations.

---
+++ Sources/GeoFeatures/Segment.swift
@@ -0,0 +1,33 @@
+//
+//  Segment.swift
+//  Pods
+//
+//  Created by Tony Stone on 11/22/16.
+//
+//
+
+import Swift
+import Foundation
+
+///
+/// Low level type to represent a segment of a line used in geometric computations.
+///
+internal class Segment<CoordinateType: Coordinate & CopyConstructable> {
+    
+    internal var c1: CoordinateType
+    internal var c2: CoordinateType
+    
+    init(c1: CoordinateType, c2: CoordinateType) {
+        self.c1 = c1
+        self.c2 = c2
+    }
+}
+
+///
+/// Segment is Equatable so it can be added to a b-tree and searched.
+///
+extension Segment: Equatable /* where CoordinateType: Equatable */ {}
+
+internal func == <CoordinateType: Coordinate & CopyConstructable>(lhs: Segment<CoordinateType>, rhs: Segment<CoordinateType>) -> Bool {
+    return false
+}
