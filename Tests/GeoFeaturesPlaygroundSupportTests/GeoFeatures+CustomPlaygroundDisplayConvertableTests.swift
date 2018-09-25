6733b8f80a79240ef7d960552e2e5070288bf790
Tony Stone <hatter24@gmail.com>
2018-09-25 14:45:36 -0700

    Fixing header license.

--- Tests/GeoFeaturesPlaygroundSupportTests/GeoFeatures+CustomPlaygroundDisplayConvertableTests.swift
+++ Tests/GeoFeaturesPlaygroundSupportTests/GeoFeatures+CustomPlaygroundDisplayConvertableTests.swift
@@ -1,10 +1,22 @@
-//
-//  GeoFeatures+CustomPlaygroundDisplayConvertableTests.swift
-//  GeoFeaturesPlaygroundSupportTests
-//
-//  Created by Tony Stone on 8/20/18.
-//
-
+///
+///  GeoFeatures+CustomPlaygroundDisplayConvertableTests.swift
+///
+///  Copyright (c) Tony Stone, All rights reserved.
+///
+///  Licensed under the Apache License, Version 2.0 (the "License");
+///  you may not use this file except in compliance with the License.
+///  You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+///  Unless required by applicable law or agreed to in writing, software
+///  distributed under the License is distributed on an "AS IS" BASIS,
+///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///  See the License for the specific language governing permissions and
+///  limitations under the License.
+///
+///  Created by Tony Stone on 8/20/18.
+///
 import XCTest
 
 #if os(OSX) || os(iOS)
