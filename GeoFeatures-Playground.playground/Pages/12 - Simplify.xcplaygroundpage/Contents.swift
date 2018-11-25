024516bb0e1f5911f23c5bff125281bbc4168d50
Tony Stone <hatter24@gmail.com>
2018-11-25 11:23:43 -0800

    Adding playground page for working with `func simplify`.

--- GeoFeatures-Playground.playground/Pages/12 - Simplify.xcplaygroundpage/Contents.swift
+++ GeoFeatures-Playground.playground/Pages/12 - Simplify.xcplaygroundpage/Contents.swift
@@ -1,7 +1,17 @@
 //: [Previous](@previous)
 
-import Foundation
+import GeoFeatures
 
-var str = "Hello, playground"
 
-//: [Next](@next)
+let point = Point([1.0, 2.0, 3.0, 2.0])
+
+point.simplify(tolerance: 1.0)
+
+
+let lineString = LineString([[100, 100], [100, 100], [150, 100], [200, 100]])
+
+let simplifiedLineString = lineString.simplify(tolerance: 1.0)
+
+type(of: simplifiedLineString)
+
+
