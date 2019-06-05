6e88c1740c21680883def6489df9b4ff5cb2e876
Ed Swiss <eswiss@comcast.net>
2019-06-04 21:33:11 -0700

    Added code to calculate the intersection of a Polygon and a MultiPoint.  Added twenty-nine related unit tests.

--- Sources/GeoFeatures/Intersection.swift
+++ Sources/GeoFeatures/Intersection.swift
@@ -270,9 +270,8 @@ fileprivate func intersectionTwoZero(_ geometry1: Geometry, _ geometry2: Geometr
 
     if let polygon = geometry1 as? Polygon, let point = geometry2 as? Point {
         return generateIntersection(point, polygon)
-//    } else if let polygon = geometry1 as? Polygon, let points = geometry2 as? MultiPoint {
-//        let intersectionMatrix = generateIntersection(points, polgyon)
-//        return intersectionMatrix.transposed()
+    } else if let polygon = geometry1 as? Polygon, let points = geometry2 as? MultiPoint {
+        return generateIntersection(points, polygon)
 //    } else if let multipolygon = geometry1 as? MultiPolygon, let point = geometry2 as? Point {
 //        let intersectionMatrix = generateIntersection(point, multipolygon)
 //        return intersectionMatrix.transposed()
