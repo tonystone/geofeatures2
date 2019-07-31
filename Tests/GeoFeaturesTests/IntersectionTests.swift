1f90cf3ba9b4b308b20f8262d6b17d6c8e4e1b48
Ed Swiss <eswiss@comcast.net>
2019-07-30 18:09:36 -0700

    Added one more unit test for the Polygon/Polygon intersection.  No code added or altered.  Time to create even harder unit tests!

--- Tests/GeoFeaturesTests/IntersectionTests.swift
+++ Tests/GeoFeaturesTests/IntersectionTests.swift
@@ -8503,6 +8503,21 @@ class IntersectionTests: XCTestCase {
         XCTAssertEqual(resultGeometry, expected)
     }
 
+    func testPolygon_Polygon_withHoles_overlapOnTwoPolygons() {
+
+        let geometry1 = Polygon([Coordinate(x: 40.0, y: 40.0), Coordinate(x: 60.0, y: 40.0), Coordinate(x: 60.0, y: 0.0), Coordinate(x: 0.0, y: 0.0), Coordinate(x: 0.0, y: 40.0), Coordinate(x: 20.0, y: 40.0), Coordinate(x: 20.0, y: 20.0), Coordinate(x: 40.0, y: 20.0), Coordinate(x: 40.0, y: 40.0)], innerRings: [[Coordinate(x: 46.0, y: 30.0), Coordinate(x: 54.0, y: 30.0), Coordinate(x: 54.0, y: 35.0), Coordinate(x: 46.0, y: 35.0), Coordinate(x: 46.0, y: 30.0)]], precision: precision, coordinateSystem: cs)
+        let geometry2 = Polygon([Coordinate(x: 0.0, y: 20.0), Coordinate(x: 0.0, y: 60.0), Coordinate(x: 60.0, y: 60.0), Coordinate(x: 60.0, y: 20.0), Coordinate(x: 40.0, y: 20.0), Coordinate(x: 40.0, y: 40.0), Coordinate(x: 20.0, y: 40.0), Coordinate(x: 20.0, y: 20.0), Coordinate(x: 0.0, y: 20.0)], innerRings: [[Coordinate(x: 6.0, y: 35.0), Coordinate(x: 6.0, y: 30.0), Coordinate(x: 14.0, y: 30.0), Coordinate(x: 14.0, y: 35.0), Coordinate(x: 6.0, y: 35.0)]], precision: precision, coordinateSystem: cs)
+
+        guard let resultGeometry = intersection(geometry1, geometry2) as? GeometryCollection else {
+            return XCTFail()
+        }
+
+        var expected  = GeometryCollection()
+        expected.append(MultiPolygon([Polygon([Coordinate(x: 40.0, y: 40.0), Coordinate(x: 60.0, y: 40.0), Coordinate(x: 60.0, y: 20.0), Coordinate(x: 40.0, y: 20.0), Coordinate(x: 40.0, y: 40.0)], innerRings: [[Coordinate(x: 46.0, y: 30.0), Coordinate(x: 54.0, y: 30.0), Coordinate(x: 54.0, y: 35.0), Coordinate(x: 46.0, y: 35.0), Coordinate(x: 46.0, y: 30.0)]]), Polygon([Coordinate(x: 0.0, y: 40.0), Coordinate(x: 20.0, y: 40.0), Coordinate(x: 20.0, y: 20.0), Coordinate(x: 0.0, y: 20.0), Coordinate(x: 0.0, y: 40.0)], innerRings: [[Coordinate(x: 6.0, y: 35.0), Coordinate(x: 6.0, y: 30.0), Coordinate(x: 14.0, y: 30.0), Coordinate(x: 14.0, y: 35.0), Coordinate(x: 6.0, y: 35.0)]])], precision: precision, coordinateSystem: cs))
+
+        XCTAssertEqual(resultGeometry, expected)
+    }
+
 //    ///
 //    /// Polygon MultiPolygon tests
 //    ///
