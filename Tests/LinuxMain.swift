0cd9308c90a2b82970078d810fed743c6302dd78
Tony Stone <hatter24@gmail.com>
2020-02-29 10:15:15 -0600

    Update linux_main removing `testMatchUnicodeEWithAccent`.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -914,7 +914,6 @@ extension TokenizerTests {
                 ("testMatchUnicodeFlagFalse", testMatchUnicodeFlagFalse),
                 ("testMatchUnicodeEPlusAccent", testMatchUnicodeEPlusAccent),
                 ("testMatchUnicodeEPlusAccentFalse", testMatchUnicodeEPlusAccentFalse),
-                ("testMatchUnicodeEWithAccent", testMatchUnicodeEWithAccent),
                 ("testMatchUnicodeEWithAccentFalse", testMatchUnicodeEWithAccentFalse),
                 ("testColumnParens", testColumnParens),
                 ("testColumnUnicodeGlobes", testColumnUnicodeGlobes),
