e9ac693c22c4b63306bf4e4415dd6cc67c87b34f
Ed Swiss <eswiss@comcast.net>
2020-06-01 20:41:43 -0700

    Added two unit tests for the covers predicate for a Polygon.

--- Tests/GeoFeaturesTests/Polygon+GeometryTests.swift
+++ Tests/GeoFeaturesTests/Polygon+GeometryTests.swift
@@ -469,5 +469,65 @@ class PolygonGeometryCoordinate2DFloatingPrecisionCartesianTests: XCTestCase {
         XCTAssertFalse(testPolygon.overlaps(multiPolygon2))
         XCTAssertFalse(testPolygon.overlaps(multiPolygon3))
     }
+
+    func testCoversTrue() {
+        let testPolygon = Polygon([[1.0, 1.0], [100.0, 100.0], [100.0, 1.0], [1.0, 1.0]], precision: precision, coordinateSystem: cs)
+
+        let point1 = Point(Coordinate(x: 100.0, y: 100.0), precision: precision, coordinateSystem: cs)
+        let point2 = Point(Coordinate(x: 50.0, y: 10.0), precision: precision, coordinateSystem: cs)
+        let multiPoint1 = MultiPoint([Point(Coordinate(x: 100.0, y: 100.0))], precision: precision, coordinateSystem: cs)
+        let multiPoint2 = MultiPoint([Point(Coordinate(x: 100.0, y: 100.0)), Point(Coordinate(x: 100.0, y: 1.0)), Point(Coordinate(x: 1.0, y: 1.0)), Point(Coordinate(x: 40.0, y: 12.0))], precision: precision, coordinateSystem: cs)
+        let lineString1 = LineString([[100.0, 100.0], [1.0, 1.0]], precision: precision, coordinateSystem: cs)
+        let lineString2 = LineString([[50.0, 1.0], [50.0, 10.0]], precision: precision, coordinateSystem: cs)
+        let linearRing1 = LinearRing([[1.0, 1.0], [100.0, 1.0], [100.0, 100.0], [1.0, 1.0]], precision: precision, coordinateSystem: cs)
+        let linearRing2 = LinearRing([[80.0, 10.0], [80.0, 20.0], [90.0, 20.0], [90.0, 10.0], [80.0, 10.0]], precision: precision, coordinateSystem: cs)
+        let multiLineString1 = MultiLineString([LineString([[100.0, 100.0], [1.0, 1.0]])], precision: precision, coordinateSystem: cs)
+        let multiLineString2 = MultiLineString([LineString([[100.0, 100.0], [80.0, 80.0]]), LineString([[70.0, 70.0], [70.0, 40.0], [80.0, 40.0]]), LineString([[40.0, 2.0], [90.0,  2.0], [90.0, 3.0]])], precision: precision, coordinateSystem: cs)
+        let polygon1 = Polygon([[1.0, 1.0], [100.0, 100.0], [100.0, 1.0], [1.0, 1.0]], precision: precision, coordinateSystem: cs)
+        let polygon2 = Polygon([[90.0, 2.0], [90.0, 10.0], [82.0, 2.0], [90.0, 2.0]], precision: precision, coordinateSystem: cs)
+        let multiPolygon = MultiPolygon([Polygon([[6.0, 1.0], [1.0, 1.0], [6.0, 6.0], [6.0, 1.0]], innerRings: []), Polygon([[60.0, 2.0], [60.0, 22.0], [80.0, 22.0], [80.0, 2.0], [60.0, 2.0]], innerRings: [[[62.0, 14.0], [62.0, 4.0], [72.0, 4.0], [72.0, 14.0], [62.0, 14.0]]])], precision: precision, coordinateSystem: cs)
+
+        XCTAssertTrue(testPolygon.covers(point1))
+        XCTAssertTrue(testPolygon.covers(point2))
+        XCTAssertTrue(testPolygon.covers(multiPoint1))
+        XCTAssertTrue(testPolygon.covers(multiPoint2))
+        XCTAssertTrue(testPolygon.covers(lineString1))
+        XCTAssertTrue(testPolygon.covers(lineString2))
+        XCTAssertTrue(testPolygon.covers(linearRing1))
+        XCTAssertTrue(testPolygon.covers(linearRing2))
+        XCTAssertTrue(testPolygon.covers(multiLineString1))
+        XCTAssertTrue(testPolygon.covers(multiLineString2))
+        XCTAssertTrue(testPolygon.covers(polygon1))
+        XCTAssertTrue(testPolygon.covers(polygon2))
+        XCTAssertTrue(testPolygon.covers(multiPolygon))
+    }
+
+    func testCoversFalse() {
+        let testPolygon = Polygon([[1.0, 1.0], [100.0, 100.0], [100.0, 1.0], [1.0, 1.0]], precision: precision, coordinateSystem: cs)
+
+        let point = Point(Coordinate(x: 100.0, y: 101.0), precision: precision, coordinateSystem: cs)
+        let multiPoint1 = MultiPoint([Point(Coordinate(x: 101.0, y: 100.0))], precision: precision, coordinateSystem: cs)
+        let multiPoint2 = MultiPoint([Point(Coordinate(x: 100.0, y: 100.0)), Point(Coordinate(x: 105.0, y: 100.0))], precision: precision, coordinateSystem: cs)
+        let lineString1 = LineString([[100.0, 100.0], [100.0, 0.0]], precision: precision, coordinateSystem: cs)
+        let lineString2 = LineString([[50.0, 50.0], [50.0, 0.0]], precision: precision, coordinateSystem: cs)
+        let linearRing = LinearRing([[101.0, 101.0], [101.0, 1.0], [1.0, 1.0], [101.0, 101.0]], precision: precision, coordinateSystem: cs)
+        let multiLineString1 = MultiLineString([LineString([[100.0, 100.0], [100.0, 101.0]])], precision: precision, coordinateSystem: cs)
+        let multiLineString2 = MultiLineString([LineString([[100.0, 100.0], [90.0, 90.0]]), LineString([[100.0, 100.0], [100.0, 101.0]]), LineString([[10.0, 2.0], [99.0, 2.0]])], precision: precision, coordinateSystem: cs)
+        let polygon1 = Polygon([[100.0, 100.0], [100.0, 101.0], [101.0, 101.0], [101.0, 100.0], [100.0, 100.0]], innerRings: [], precision: precision, coordinateSystem: cs)
+        let polygon2 = Polygon([[-1000.0, -1000.0], [-1000.0, 1000.0], [1000.0, 1000.0], [1000.0, -1000.0], [-1000.0, -1000.0]], innerRings: [[[0.0, 0.0], [100.0, 0.0], [100.0, 100.0], [0.0, 100.0], [0.0, 0.0]]], precision: precision, coordinateSystem: cs)
+        let multiPolygon = MultiPolygon([Polygon([[6.0, 1.0], [1.0, 1.0], [1.0, 6.0], [3.5, 6.0], [6.0, 6.0], [6.0, 1.0]], innerRings: [[[5.0, 2.0], [5.0, 3.0], [3.5, 3.5], [2.0, 3.0], [2.0, 2.0], [5.0, 2.0]]]), Polygon([[10.0, 1.0], [8.0, 1.0], [8.0, 10.0], [10.0, 10.0], [10.0, 1.0]], innerRings: [])], precision: precision, coordinateSystem: cs)
+
+        XCTAssertFalse(testPolygon.covers(point))
+        XCTAssertFalse(testPolygon.covers(multiPoint1))
+        XCTAssertFalse(testPolygon.covers(multiPoint2))
+        XCTAssertFalse(testPolygon.covers(lineString1))
+        XCTAssertFalse(testPolygon.covers(lineString2))
+        XCTAssertFalse(testPolygon.covers(linearRing))
+        XCTAssertFalse(testPolygon.covers(multiLineString1))
+        XCTAssertFalse(testPolygon.covers(multiLineString2))
+        XCTAssertFalse(testPolygon.covers(polygon1))
+        XCTAssertFalse(testPolygon.covers(polygon2))
+        XCTAssertFalse(testPolygon.covers(multiPolygon))
+    }
 }
 
