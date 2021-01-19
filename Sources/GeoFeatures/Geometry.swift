3d43c038414dcdcd632f8cb5806ee798e9cf37c5
Ed Swiss <eswiss@comcast.net>
2021-01-18 21:16:18 -0800

    Added several headers to functions in the Geometry.swift file.

--- Sources/GeoFeatures/Geometry.swift
+++ Sources/GeoFeatures/Geometry.swift
@@ -185,18 +185,50 @@ public func == (lhs: Geometry, rhs: Geometry) -> Bool {
 ///
 extension Geometry {
 
+    ///
+    /// Checks whether two geometries are topologically equivalent.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self and other are topologically equivalent.
+    ///
     public func equals(_ other: Geometry) -> Bool {   // FIXME: equals is implemented but is still required to be implemented for a class implementing Geometry.  Figure out why it is.
         return relate(other, pattern: "T*F**FFF*")
     }
 
+    ///
+    /// Checks whether two geometries touch or overlap in any way.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self and other are completely disjoint.
+    ///
     public func disjoint(_ other: Geometry) -> Bool {
         return relate(other, pattern: "FF*FF****")
     }
 
+    ///
+    /// Checks whether two geometries touch or overlap in any way.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self and other touch in any way.
+    ///
     public func intersects(_ other: Geometry) -> Bool {
         return !disjoint(other)
     }
 
+    ///
+    /// Checks whether two geometries have at least one point in common, but their interiors do not intersect.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self and other have at least one point in common, but their interiors do not intersect.
+    ///
     public func touches(_ other: Geometry) -> Bool {
 
         if self.dimension == .zero && other.dimension == .zero {
@@ -205,6 +237,15 @@ extension Geometry {
         return relate(other, pattern: "FT*******") || relate(other, pattern: "F**T*****") || relate(other, pattern: "F***T****")
     }
 
+    ///
+    /// Checks whether two geometries have some but not all interior points in common, and the dimension of the intersection is less than
+    /// that of at least one of them.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self touches the interior of other, but the dimension of the intersection is less than at least one of them.
+    ///
     public func crosses(_ other: Geometry) -> Bool {
 
         if self.dimension == .one && other.dimension == .one {
@@ -223,14 +264,39 @@ extension Geometry {
         return false
     }
 
+    ///
+    /// Checks whether self is completely contained within another geometry, but the interiors must touch.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self is completely contained within other with the interiors touching.
+    ///
     public func within(_ other: Geometry) -> Bool {
         return relate(other, pattern: "T*F**F***")
     }
 
+    ///
+    /// Checks whether self completely contains another geometry and the two interiors intersect.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self completely contains other and the two interiors intersect.
+    ///
     public func contains(_ other: Geometry) -> Bool {
         return other.within(self)
     }
 
+    ///
+    /// Checks whether two geometries have some but not all points in common.  Both dimensions must be the same.  The intersection of the interiors
+    /// must have the same dimension as the geometries themselves.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self, other, and their intersection have the same dimension, but neither geometry is contained in the other.
+    ///
     public func overlaps(_ other: Geometry) -> Bool {
 
         if self.dimension == .zero && other.dimension == .zero ||
@@ -245,22 +311,55 @@ extension Geometry {
         return false
     }
 
+    ///
+    /// Checks whether self touches the other geometry at at least one point and no point of the other geometry lies on the exterior of self.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self touches the other geometry at at least one point and no point of the other geometry lies on the exterior of self.
+    ///
     public func covers(_ other: Geometry) -> Bool {
 
         return (relate(other, pattern: "T*****FF*") || relate(other, pattern: "*T****FF*") || relate(other, pattern: "***T**FF*") || relate(other, pattern: "****T*FF*"))
     }
 
+    ///
+    /// Checks whether self touches the other geometry at at least one point and no point of self lies on the exterior of the other geometry.
+    ///
+    /// - Parameters:
+    ///     - other: Some geometry
+    ///
+    /// - Returns: True if self touches the other geometry at at least one point and no point of self lies on the exterior of the other geometry.
+    ///
     public func coveredby(_ other: Geometry) -> Bool {
 
         return (relate(other, pattern: "T*F**F***") || relate(other, pattern: "*TF**F***") || relate(other, pattern: "**FT*F***") || relate(other, pattern: "**F*TF***"))
     }
 
+    ///
+    /// Checks whether the intersection matrix formed by self and the other geometry satisfy the given string pattern.
+    ///
+    /// - Parameters:
+    ///     - other:   Some geometry
+    ///     - pattern: A string that defines an intersection matrix pattern.
+    ///
+    /// - Returns: True if the intersection matrix formed by self and the other geometry satisfy the given string pattern.
+    ///
     public func relate(_ other: Geometry, pattern: String) -> Bool {
         let matrix = calculateIntersectionMatrix(other)
 
         return matrix.matches(pattern)
     }
 
+    ///
+    /// Calculate the intersection matrix formed by self and the other geometry.
+    ///
+    /// - Parameters:
+    ///     - other:   Some geometry
+    ///
+    /// - Returns: The intersection matrix formed by self and the other geometry.
+    ///
     fileprivate func calculateIntersectionMatrix(_ other: Geometry) -> IntersectionMatrix {
         return IntersectionMatrix.generateMatrix(self, other)
     }
